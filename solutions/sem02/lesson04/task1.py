284598d795457b7a2a8dcac5723dc90de2a72236
dandandil <daniil182403@gmail.com>
2026-03-13 22:36:52 +0300

    t1 t2 ready

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,54 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    y = image.shape[0]
+    x = image.shape[1]
+
+    if image.ndim == 3:
+        z = image.shape[2]
+
+        pad = np.zeros(shape=(y + 2 * pad_size, x + 2 * pad_size, z), dtype=image.dtype)
+        pad[pad_size : pad_size + y, pad_size : pad_size + x, :] = image
+
+        return pad
+
+    pad = np.zeros(shape=(y + 2 * pad_size, x + 2 * pad_size), dtype=np.uint8)
+    pad[pad_size : pad_size + y, pad_size : pad_size + x] = image
+
+    return pad
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size % 2 == 0 or kernel_size < 1:
+        raise ValueError
+
+    if kernel_size == 1:
+        return image
+
+    pad = kernel_size // 2
+    padded = pad_image(image, pad)
+
+    sum_shape = list(padded.shape)
+    sum_shape[0] += 1
+    sum_shape[1] += 1
+
+    sum = np.zeros(sum_shape, dtype=np.uint64)
+    sum[1:, 1:] = padded.cumsum(axis=0).cumsum(axis=1)
+
+    kernel_sum = (
+        sum[kernel_size:, kernel_size:]
+        - sum[:-kernel_size, kernel_size:]
+        - sum[kernel_size:, :-kernel_size]
+        + sum[:-kernel_size, :-kernel_size]
+    )
+
+    return np.uint8(kernel_sum // (kernel_size * kernel_size))
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,26 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    counts = np.zeros(256, dtype=int)
+    np.add.at(counts, image.flatten(), 1)
+
+    padded = np.zeros(256 + 2 * threshold, dtype=int)
+    padded[threshold : threshold + 256] = counts
+
+    S = np.zeros(padded.size + 1, dtype=int)
+    S[1:] = np.cumsum(padded)
+
+    window_size = 2 * threshold - 1
+    left = np.arange(1, 257, dtype=int)
+    right = left + window_size
+    smoothed = S[right] - S[left]
+
+    smoothed[counts == 0] = -1
+
+    dominant = np.uint8(np.argmax(smoothed))
+    ratio = float(smoothed[dominant] / image.size)
+
+    return dominant, ratio
