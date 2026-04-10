4f8d03e749c29424dc6c894d3fd833b5046c5410
dandandil <daniil182403@gmail.com>
2026-04-10 23:09:23 +0300

    les07 t1 t2 ready

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,8 +13,81 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    figure = plt.figure(figsize=(8, 8))
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=space)
+
+    axis_scatter = figure.add_subplot(grid[:-1, 1:])
+
+    axis_vert = figure.add_subplot(
+        grid[:-1, 0],
+        sharey=axis_scatter,
+    )
+
+    axis_hor = figure.add_subplot(
+        grid[-1, 1:],
+        sharex=axis_scatter,
+    )
+
+    axis_scatter.scatter(abscissa, ordinates, color="cornflowerblue", alpha=0.5)
+
+    if diagram_type == "hist":
+        axis_hor.hist(
+            abscissa,
+            bins=50,
+            color="cornflowerblue",
+            density=True,
+            alpha=0.5,
+        )
+
+        axis_vert.hist(
+            ordinates,
+            bins=50,
+            color="cornflowerblue",
+            orientation="horizontal",
+            density=True,
+            alpha=0.5,
+        )
+
+    if diagram_type == "violin":
+        violin_parts_hor = axis_hor.violinplot(
+            abscissa,
+            orientation="horizontal",
+        )
+
+        violin_parts_vert = axis_vert.violinplot(
+            ordinates,
+        )
+
+        for violin_parts in (violin_parts_hor, violin_parts_vert):
+            for body in violin_parts["bodies"]:
+                body.set_facecolor("cornflowerblue")
+                body.set_edgecolor("blue")
+
+            for part in violin_parts:
+                if part == "bodies":
+                    continue
+                violin_parts[part].set_edgecolor("cornflowerblue")
+
+    if diagram_type == "box":
+        axis_hor.boxplot(
+            abscissa,
+            orientation="horizontal",
+            patch_artist=True,
+            boxprops=dict(facecolor="lightsteelblue"),
+            medianprops=dict(color="k"),
+        )
+
+        axis_vert.boxplot(
+            ordinates,
+            patch_artist=True,
+            boxprops=dict(facecolor="lightsteelblue"),
+            medianprops=dict(color="k"),
+        )
+
+    axis_hor.invert_yaxis()
+    axis_vert.invert_xaxis()
+
+    plt.show()
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,56 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+from json import load
+from typing import Any
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def read_data(path: Any = "./data/medic_data.json") -> Any:
+    with open(path) as file:
+        data = load(file)
+
+    return data
+
+
+def get_amounts(data: np.array) -> np.array:
+
+    data_amnt = np.array([data.count("I"), data.count("II"), data.count("III"), data.count("IV")])
+
+    return data_amnt
+
+
+def visualize_data(before: np.array, after: np.array) -> None:
+
+    figure, axis = plt.subplots(figsize=(16, 8))
+
+    stages = ["I", "II", "III", "IV"]
+    x = np.arange(len(stages))
+    width = 0.35
+
+    axis.bar(x - width / 2, before, width, label="до установки импланта", color="red")
+
+    axis.bar(x + width / 2, after, width, label="после установки импланта", color="green")
+
+    axis.set_title("Степени митральной недостаточности", fontsize=16, fontweight="bold")
+
+    axis.set_ylabel("Количество пациентов", fontweight="bold")
+    axis.set_xticks(x)
+    axis.set_xticklabels(stages, fontweight="bold")
+    axis.tick_params(axis="both", labelcolor="dimgray")
+
+    axis.legend(fontsize=12)
+
+    plt.show()
+
+
+def main():
+    data = read_data("./data/medic_data.json")
+
+    before_amounts = get_amounts(data["before"])
+    after_amounts = get_amounts(data["after"])
+
+    visualize_data(before_amounts, after_amounts)
+
+
+if __name__ == "__main__":
+    main()
